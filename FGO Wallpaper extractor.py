ddb73bca6ea3cc9409e5d5bc622540823e146978
Clavin D'souza <31620736+WeebMogul@users.noreply.github.com>
2020-06-13 13:25:59 +0400

    Update 1.1
    - Added new June artwork.
    - June collections are now merged into one.

--- FGO Wallpaper extractor.py
+++ FGO Wallpaper extractor.py
@@ -1,6 +1,17 @@
 import os
 import requests
 from tqdm import tqdm
+from cv2 import cv2
+import shutil
+
+'''
+1. Create directory.
+2. Put all the numbers in an array or list.
+3. Attach no. to existing link.
+4. Extract the link using the number 
+5. Place in directory 
+6. Convert to 1920 x 1080 resolution using ffmpeg
+'''
 
 # For extracting the images 
 def image_extractor(url, img_names, folder_dir):
@@ -46,32 +57,67 @@ def get_dem_wallpapers(fold_name,folder,img,wall_no):
     wall_names = [str(i).zfill(2) for i in range(1,wall_no)]
     image_extractor(img, wall_names, new_folder_path)
 
-img_links = ['https://5th.fate-go.jp/assets/img/slide_0504_jzgkaw3b/s01.jpg','https://5th.fate-go.jp/assets/img/slide_0525_r6dwy7nt/s01.jpg','https://5th.fate-go.jp/assets/img/slide_0603_d486tjek/s01.jpg']
-wall_no = [12,7,9]
-fold_name = ['April Collection','May Collection','June Collection']
+def merge_folders(folder):
+
+    dirs = os.listdir(folder)
+    
+    folds = ['June Collection 1',
+            'June Collection 2']
+
+    if not os.path.exists(os.path.join(folder,'June Collection')):
+        os.mkdir(os.path.join(folder,'June Collection'))
+        for i in dirs:
+            if i in folds:
+                shutil.move(os.path.join(folder,i),os.path.join(folder,'June Collection'))
+    else :
+        pass
+
+# For converting to 1920 x 1080p 
+'''
+def resolution_convert(path, arr):
+
+    for i in tqdm(range(len(arr))):
+
+        file_no = 's'+arr[i]+'.jpg'
+        img_name = os.path.join(path, file_no)
+        img = cv2.imread(img_name,cv2.IMREAD_UNCHANGED)
+        
+        new = cv2.resize(img, (1920, 1080), interpolation=cv2.INTER_AREA)
+        new_file = 's'+arr[i]+'_upscaled'+'.jpg'
+        folder_path = os.path.join(path, new_file)
+        cv2.imwrite(folder_path, new)
+'''
+
+img_links = ['https://5th.fate-go.jp/assets/img/slide_0504_jzgkaw3b/s01.jpg',
+            'https://5th.fate-go.jp/assets/img/slide_0525_r6dwy7nt/s01.jpg',
+            'https://5th.fate-go.jp/assets/img/slide_0603_d486tjek/s01.jpg',
+            'https://5th.fate-go.jp/assets/img/slide_0613_g6z3i5ts/s01.jpg']
+
+wall_no = [12,7,9,8]
+fold_name = ['April Collection','May Collection','June Collection 1', 'June Collection 2']
 
 parent_dir = os.getcwd()
 main_folder_name = "FGO 5th anniversary wallpapers"
 folder = os.path.join(parent_dir, main_folder_name)
 
-months = 3
+months = 4
 
 if not os.path.exists(folder):
 
     os.mkdir(folder)
 
     for i in range(0,months):
-
        get_dem_wallpapers(fold_name[i],folder,img_links[i],wall_no[i])
        print(f"\n Downloaded the {fold_name[i]} wallpapers \n")
-
 else :
-
     for i in range(0,months):
         
         if os.path.exists(os.path.join(folder,fold_name[i])):
             print(f'The {fold_name[i]} exists')
+           #  merge_folders(folder)
+
         else :
             get_dem_wallpapers(fold_name[i],folder,img_links[i],wall_no[i])
             print(f"\n Downloaded the {fold_name[i]} wallpapers \n")
+    merge_folders(folder)
 
